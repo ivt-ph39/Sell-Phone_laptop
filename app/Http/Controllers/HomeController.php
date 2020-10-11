5a032d0432dce9ec9cb1bad7d4c1dc6b2f5c7be2
NoQuang1998 <quangdnn98@gmail.com>
2020-10-12 02:53:12 +0700

    update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,10 +23,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        echo 'test';
-        echo 'test';
-        echo 'test';
-        echo 'test';
         return view('home');
     }
 }
